5a5379178823c9a05c1b07da48ccb886537b306e
gvjacob <jacob.g@husky.neu.edu>
2018-09-16 22:04:21 -0400

    Fixed names for task 2

--- 3/server-for-given-2spread.py
+++ 3/server-for-given-2spread.py
@@ -1,55 +1,47 @@
-import re, csv
-from oslash import Maybe, Just, Nothing
+import re
+import csv
 from ast import literal_eval as make_tuple
 
 class Spreadsheet:
-    table = {}
-
     def __init__(self):
-        self.table = {}
+        self.__table = {}
 
     def create_spreadsheet(self, path):
-        """ Create coordinates and formulae mapping from csv """
+        """ 
+        Create coordinates and formulae mapping from csv that has only two columns
+        separated by semi-colons instead of commas. First contains coordinates as 
+        tuples and the second valid formulae. Any invalid values are skipped.
+        """
         with open(path, newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter = ';')
+            reader = csv.reader(csvfile, delimiter=';')
             for row in reader:
-                print(row)
                 try:
-                    self.table[make_tuple(row[0])] = row[1]
+                    self.__table[make_tuple(row[0])] = row[1]
                 except:
                     continue
 
     def get_value(self, cell):
-        """ Get value of cell """
-        if cell in self.table:
-            return Just(self.evaluate(self.table[cell]))
+        """ Get value of cell. Otherwise, return None """
+        if cell in self.__table:
+            return self.__evaluate(self.__table[cell])
         else:
-            return Nothing()
+            return None
 
     def put_value(self, cell, value):
         """ Update cell with formula """
-        if cell in self.table:
-            self.table[cell] = value
+        if cell in self.__table:
+            self.__table[cell] = value
 
-    def evaluate(self, value):
+    def __evaluate(self, value):
         """ Evaluate a formula's value """
         formula = value
-        tuple_mappings = self.extract_refs(value)
+        tuple_mappings = self.__extract_refs(value)
         for key, value in tuple_mappings:
-            formula = formula.replace(key, str(self.evaluate(value)))
-        return eval(formula, {'__builtins__':{}})
-    
-    def extract_refs(self, value):
+            formula = formula.replace(key, str(self.__evaluate(value)))
+        return eval(formula, {'__builtins__': {}})
+
+    def __extract_refs(self, value):
         """ Extract cell references from value """
         tuple_format = r"\([-+]?\d+\,\s*[-+]?\d+\)"
         tuples = re.findall(tuple_format, value)
-        return list(map(lambda tuple: (tuple, self.table[make_tuple(tuple)]), tuples))
-
-spreadsheet = Spreadsheet()
-spreadsheet.create_spreadsheet('sample-spreadsheet.csv')
-print(spreadsheet.table)
-print(spreadsheet.get_value((0, 0)))
-spreadsheet.put_value((0,0), " 2 + 2")
-print(spreadsheet.get_value((0, 0)))
-print(spreadsheet.get_value((0, 1)))
-print(spreadsheet.get_value((0, 2)))
+        return list(map(lambda tuple: (tuple, self.__table[make_tuple(tuple)]), tuples))
