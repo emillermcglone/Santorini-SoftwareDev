e3ba4649aca3a3df1291a450cdab754523b40753
gvjacob <jacob.g@husky.neu.edu>
2018-09-17 14:33:05 -0400

    client

--- 3/client.py
+++ 3/client.py
@@ -2,45 +2,45 @@ import sys
 import fileinput
 import json
 
-class Spreadsheet:
-    def __init__(self, rectangle):
-        pass
+from spreadsheet import *
+from oslash import Just, Maybe, Nothing
+from splitstream import splitfile
 
-    def get_value(self, x, y):
-        pass
+spreadsheets = {}
 
-    def update(self, x, y, formula):
-        pass
+def write_to_output(func, output):
+    def go():
+        return func()
+    
+    if
 
-class Formula:
-    def __init__(self, string_formula):
-        pass
+def parse_json_formula(jf):
+    json_value = json.loads(jf)
+    if isinstance(json_value, int) and json_value >= 0:
+        return str(json_value)
+    elif isinstance(json_value, list) and json_value[0] == ">":
+        return str((json_value[1], json_value[2]))
+    else:
+        return "(" + parse_json_formula(json_value[0]) + json_value[1] + parse_json_formula(json_value[2]) + ")"
 
-    def get_references(self):
-        """ Get all cell references in this formula"""
-        pass
-
-    def evaluate(self, dictionary):
-        """ Evaluate formula with given mappings from references to values """
-        pass
 
 def at_request(req):
-    pass
+    spreadsheet = spreadsheets[req[1]]
+    value = spreadsheet.get_value(req[2], req[3])
+
 
 def set_request(req):
-    pass
+    spreadsheet = spreadsheets[req[1]]
+    spreadsheet.update(req[2], req[3], Formula(parse_json_formula(req[4])))
 
 def create_request(req):
+    if req[2] in spreadsheets:
+        return
+    spreadsheet = Spreadsheet(req[2])
+    spreadsheets[req[1]] = spreadsheet
 
-    pass
-
-def pass_json_inputs(input_source):
-    try:
-        request = json.loads(input_source.readline())
-        request_type = request[0]
-    except: 
-        pass
-
+def handle_request(req):
+    request_type = req[0]
     if request_type == "sheet":
         create_request(request)
     elif request_type == "set":
@@ -51,5 +51,8 @@ def pass_json_inputs(input_source):
         pass
 
 
-if __name__ == "__main__":
-    main()
+def pass_json_inputs(input_source):
+    try:
+        request = json.loads(input_source.readline())
+    except:
+        return
