75d457b964d27be189a1a7800de1cb4f4aa1caa3
gvjacob <jacob.g@husky.neu.edu>
2018-09-17 11:45:59 -0400

    Cleaner server code for task 1

--- 3/3.py
+++ 3/3.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python
 
-import socket
+import socketserver
 import importlib
-import sys
-
 import io
+import sys
 
 sys.path.append('../2/Aux')
 two = __import__('2')
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 8000
-BUFFER_SIZE = 1024
+class TCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
 
-connection, address = s.accept()
-while True:
-    data = connection.recv(BUFFER_SIZE).decode('utf-8')
-    if not data: break
-    io_buffer = io.StringIO(data)
-    connection.send(two.format_json_inputs(io_buffer).encode('utf-8'))
-connection.close()
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        json_inputs = self.data.decode('utf-8')
+        io_buffer = io.StringIO(json_inputs)
+
+        # send back JSON values with reverse positions
+        self.request.sendall(two.format_json_inputs(io_buffer).encode('utf-8'))
+
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 8000
+
+    # Create the server, binding to localhost on port 8000
+    with socketserver.TCPServer((HOST, PORT), TCPHandler) as server:
+        # Activate the server; this will keep running until interrupted with Ctrl-C
+        server.serve_forever()
