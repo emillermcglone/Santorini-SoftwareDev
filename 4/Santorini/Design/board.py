bcff963b2fbf52a3188ff2fc488b299d634efd35
gvjacob <jacob.g@husky.neu.edu>
2018-09-24 15:52:42 -0400

    Changed specs

--- 4/Santorini/Design/board.py
+++ 4/Santorini/Design/board.py
@@ -1,14 +1,18 @@
 from abc import ABC, abstractmethod
 
 """
-Board is made of a zero-indexed 2D list of Cells for a Santorini game. 
+Board is made of a zero-indexed 2D list of Cells for a Santorini game,
+and manages the placement of the game's pieces. 
 
 Cell is one of (Floor, Level) and (Worker, Level)
 Floor is an empty class
-Worker has id: int, and position: (x, y)
+Worker has id: N, and position: (x, y)
+N is a natural number
 Level ranges from 0 to 4 inclusive
 
-Rule is a (board: [[Cell, ...] ...], worker: int, move_direction: Direction, build_direction: Direction) -> bool
+Rule is a function (board: [[Cell, ...] ...], worker: int, move_direction: Direction, build_direction: Direction) -> bool
+that returns True if move is permitted and False otherwise
+
 Direction is one of 'N', 'E', 'S', 'W', 'NE', 'NW', 'SE', 'SW'
 
 MoveError is one of:
@@ -26,43 +30,66 @@ BuildError is one of:
 """
 
 
-class Board(ABC):
+class Floor:
+    pass
 
+
+class Board(ABC):
+    @abstractmethod
     def __init__(self, win_callback, win_condition, rules, width=6, height=6):
         """
         Initialize board with the given dimensions, 6 x 6 by default, and
         list of game rules.
 
         @param win_callback: (worker: Worker) -> void, callback function to call when win_condition is met
-        @param win_condition: condition for a win
-        @param rules: list of Rule validating each move and build
+        @param win_condition: (board: [[Cell, ...] ...])
+        @param rules: list of Rules validating each move and build
         @param width: number of cells horizontally
         @param height: number of cells vertically
         """
-        pass
+        self.win_callback = win_callback
+        self.win_condition = win_condition
+        self.rules = rules
+        self.board = [[(Floor(), 0)] * width] * height
 
     @abstractmethod
     def place_worker(self, worker, position):
         """ 
-        Place worker on start position
+        Place worker on start position.
 
         @param worker: id of worker to be placed
         @param position: (x, y) coordinates
         @raise OutOfBounds: given position is out of the board
         @raise BlockingWorker: another worker is on given position 
         """
-
         pass
 
-    @abstractmethod
     def move_and_build(self, worker, move_direction, build_direction):
         """ 
-        Move worker and build in the given directions.
+        Move worker, build in the given directions, and evoke
+        win callback if game has been won.
 
         @param worker: id of worker
         @move_direction: Direction for move
         @build_direction: Direction for build
         @raise MoveError: invalid move
         @raise BuildError: invalid build
+        """
+        id = self.__move_and_build__(worker, move_direction, build_direction)
+        if id >= 0:
+            self.win_callback(id)
+
+    @abstractmethod
+    def __move_and_build__(self, worker, move_direction, build_direction):
+        """ 
+        Move worker and build in the given directions. Check if
+        game has been won with the move.
+
+        @param worker: id of worker
+        @move_direction: Direction for move
+        @build_direction: Direction for build
+        @raise MoveError: invalid move
+        @raise BuildError: invalid build
+        @return: -1 if move is successful and game continues, N representing winner's id otherwise
         """
         pass
