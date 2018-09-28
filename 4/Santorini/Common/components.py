87f15f68c047515dadb6c5075d0de36483900e3b
gvjacob <jacob.g@husky.neu.edu>
2018-09-28 10:16:33 -0400

    Changed components interface

---
+++ 4/Santorini/Common/components.py
@@ -0,0 +1,173 @@
+""" 
+Common data and knowledge among administrative components and players
+in a Santorini game.
+
+Common data include physical game pieces, the rules of the game,
+and the player interface.
+
+N is a natural number. 
+"""
+
+from abc import ABC, abstractmethod
+
+class Cell(ABC):
+    """
+    Individual cell element with height on a Santorini board.
+    """
+
+    @abstractmethod
+    def __init__(self, height = 0):
+        """
+        Initialize cell with height, defaults to 0.
+
+        :param height: 0 to 4 inclusive
+        :raise AttributeError: height is not between 0 and 4 inclusive        
+        """
+        pass
+
+    @property
+    def height(self):
+        """
+        Height of cell.
+        """
+        pass
+
+    @height.setter
+    def height(self, new_height):
+        """
+        Set cell's height to new_height.
+
+        :param new_height: the new height
+        :raise AttributeError: if height is not from 0 to 4
+        """
+        pass
+
+class Height(Cell):
+    """
+    Height of a building.
+    """
+
+    def __init__(self, height = 0):
+        """
+        Initialize with height of building.
+
+        :param height: N, height of building, defaults to 0
+        :raise AttributeError: if height is not from 0 to 4
+        """
+        pass
+
+class Worker(Cell):
+    """
+    Worker of a Santorini board whose height represents which floor
+    it is on.
+    """
+
+    def __init__(self, id, position, height = 0):
+        """
+        Initialize with id, position, and height of building the worker is on.
+
+        :param id: N, id of Worker
+        :param position: (N, N), the position of Worker
+        :param height: N, height of building worker is on, defaults to 0
+        :raise AttributeError: if height is not from 0 to 4        
+        """
+        pass
+
+
+class Rules(ABC):
+    """
+    Set of rules for a Santorini game which both the administrative components
+    and players can use to validate their moves before making them.
+    """
+    
+    @abstractmethod
+    def __init__(self, move_rules, build_rules):
+        """
+        Initalize with list of Rule for both moving and building. 
+        Rule is a function (board: [[Cell, ...] ...], worker: N, direction: Direction) -> bool
+  
+        :param move_rules: [Rule, ...], list of rules for move
+        :param build_rules: [Rule, ...], list of rules for build
+        """
+        pass
+
+    @abstractmethod
+    def check_move(self, board, worker, move_direction):
+        """
+        Check if the move is valid.
+
+        :param board: [[Cell, ...] ...], zero-indexed 2D list of Cells
+        :param worker: N, id of worker
+        :param move_direction: Direction, direction for move
+        """
+        pass
+
+    @abstractmethod
+    def check_build(self, board, worker, build_direction):
+        """
+        Check if the build is valid.
+
+        :param board: [[Cell, ...] ...], zero-indexed 2D list of Cells
+        :param worker: N, id of worker
+        :param build_direction: Direction, direction for build
+        """
+        pass
+
+class Player(ABC):
+    """
+    Player component for Santorini, and intermediary between administrative
+    components and the player AI.
+    """
+
+    @abstractmethod
+    def __init__(self, id, worker1_id, worker2_id):
+        """"
+        Initialize the Player with id, worker1_id, and worker2_id.
+
+        :param id: N, id of Player
+        :param worker1_id: N, the id of the Player's first worker 
+        :param worker2_id: N, the id of the Player's second worker
+        """
+        pass
+
+    @abstractmethod
+    def place_worker(self, board):
+        """
+        Provide coordinates to place one of the workers on the given board.
+
+        :param board: [[Cell, ...], ...], the current board of the game
+        :return: (N, (N, N)), the worker id, and the x and y coordinates
+        """
+        pass
+
+    @abstractmethod
+    def prompt_move(self, board, worker1, worker2):
+        """
+        Prompt the player for their move turn. 
+
+        :param board: [[Cell, ...], ...], the current board of the game
+        :param worker1: Worker, the first worker
+        :param worker2: Worker, the second worker
+        :return: (N, Direction), the worker id, move direction
+        """
+        pass
+
+    @abstractmethod
+    def prompt_build(self, board, worker):
+        """
+        Prompt the player for their build turn. 
+
+        :param board: [[Cell, ...], ...], the current board of the game
+        :param worker: Worker, the worker that was moved in that turn
+        :return: Direction, build direction
+        """
+        pass
+
+    @abstractmethod
+    def game_over(self, win):
+        """
+        Notify player that game has ended.
+
+        :param win: bool, True if this Player won the game, False otherwise
+        """
+        pass
--- 4/Santorini/Design/board.py
+++ 4/Santorini/Design/board.py
@@ -1,30 +1,14 @@
 """
-Board is made of a zero-indexed 2D list of Cells for a Santorini game,
-and manages the placement of the game's pieces. 
+Board is made of a zero-indexed 2D list of Cell for a Santorini game,
+and manages the placement of the game's pieces and its buildings. 
 
-Cell is one of Height or Worker
+Cell is either Height or Worker
 Height signifies a building floor: N, 0 to 4 inclusive
 Worker has id: N, position: (N, N), and height: N, 0 to 4 inclusive
 N is a natural number
 Height of 0 signifies the ground floor 
 
-Rule is a function (board: [[Cell, ...] ...], worker: int, move_direction: Direction, build_direction: Direction) -> bool
-that returns True if move is permitted and False otherwise
-
 Direction is one of 'N', 'E', 'S', 'W', 'NE', 'NW', 'SE', 'SW'
-
-MoveError is one of:
-    BlockingWorker: another worker on coordinates
-    HeightUnreachable: floor is more than one story higher
-    OutOfBounds: direction leads to off the board
-    StandingOnCoordinates: can't move on where worker is standing
-    HeightFour: can't move to the fourth level of a building
-
-BuildError is one of:
-    BlockingWorker: another worker on coordinates
-    OutOfBounds: direction leads to off the board
-    StandingOnCoordinates: can't build where a worker is
-    HeightFour: can't build above the fourth floor
 """
 
 import copy
@@ -58,7 +42,7 @@ class Board(ABC):
     @property
     def board(self):
         """
-        Provide a copy of the board representing state of game.
+        Provide a deep copy of the board representing state of game.
 
         :return: [[Cell, ...] ...], the state of the game
         """
@@ -71,32 +55,25 @@ class Board(ABC):
 
         :param worker: N, id of worker to be placed
         :param position: (N, N), coordinates
-        :raise PlaceOutOfBounds: given position is out of the board
-        :raise PlaceBlockingWorker: another worker is on given position 
         """
-        try: 
-            cell = self.board[position[0], position[1]]
-            if isinstance(cell, Worker):
-                raise PlaceBlockingWorker("Another worker on given position")
-        except IndexError:
-            raise PlaceOutOfBounds("Coordinates are outside of the board.")
-
-        self.board[position[0], position[1]] = Worker(worker, position)
-        self.workers[worker] = position
+        pass
 
     @abstractmethod
-    def move_and_build(self, worker, move_direction, build_direction):
+    def move(self, worker, move_direction):
         """ 
-        Move worker and build in the given directions if rules 
-        are satisfied.
+        Move worker to given direction if rules are satisfied.
 
         :param worker: N, id of worker
         :param move_direction: Direction, direction for move
-        :param build_direction: Direction, direction for build
-        :raise MoveError: invalid move
-        :raise BuildError: invalid build
         """
-        if self.rules.check(self.board, worker, move_direction, build_direction):
-            
-            
+        pass
 
+    @abstractmethod
+    def build(self, worker, build_direction):
+        """
+        Build a floor in the given direction if rules are satisfied.
+
+        :param worker: N, id of worker
+        :param build_direction: Direction, direction for build
+        """
+        pass
