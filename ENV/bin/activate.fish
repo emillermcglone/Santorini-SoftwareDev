bd3031413d389910db9e4b4407f1870e55595acb
gvjacob <jacob.g@husky.neu.edu>
2018-10-04 00:37:58 -0400

    relocatable

--- ENV/bin/activate.fish
+++ ENV/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/gino/Developer/gija-emmi/ENV"
+set -gx VIRTUAL_ENV "/Users/gino/Developer/gija-emmi/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
