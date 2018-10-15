8f600bf97c1a0268cebb383e1836812f394dbd94
gvjacob <jacob.g@husky.neu.edu>
2018-10-15 10:54:30 -0400

    place strategies

--- Santorini/Common/place_strategies.py
+++ Santorini/Common/place_strategies.py
@@ -4,58 +4,79 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from math import hypot
 from functools import reduce
 
-from Design.strategy import PlaceStrategy
+from Design.strategy import TurnStrategy, PlaceStrategy
 from Common.components import *
 
+class StrategyBackup(TurnStrategy):
+    """
+    Strategy with a list of turn strategies.
+    """
+    def __init__(self, *args):
+        """
+        Initialize strategy with list of strategies
 
-"""
-A PlaceStrategy is a (IQueryBoard, [N, ...], IRules) -> (N, N)
-which takes in the query board, list of player's workers, and the rules of the game.
+        :param args: (TurnStrategy, ...), strategies to be used
+        """
+        self.strategies = list(args)
 
-It returns the coordinates to place a new worker on and
-raises a ValueError if placing is impossible according to strategy.
-"""
 
-def place_first_diagonal_point(board, workers, rules):
+    def strategize(self, board, workers, rules):
+        """
+        Apply each strategy sequentially until a valid turn specification is given.
+        """
+        for strategy in self.strategies:
+            try:
+                specification = strategy.strategize(board, workers, rules)
+                return specification
+            except:
+                continue
+        raise ValueError("No valid strategy to apply")
+        
+
+class PlaceOnFirstDiagonalPoint(StrategyBackup, PlaceStrategy):
     """
     Place the worker on the first point of the diagonal that is still free, 
     starting from (0,0).
     """
-    coordinates = (0, 0)
+
+    def strategize(self, board, workers, rules):
+        coordinates = (0, 0)
     
-    while True:
-        if rules.check_place(*coordinates):
-            return coordinates
-        coordinates = Direction.SE(*coordinates)
+        while True:
+            if rules.check_place(*coordinates):
+                return coordinates
+            coordinates = Direction.SE(*coordinates)
 
-
-def farthest_away_from_opponent(board, workers, rules):
+class FarthestFromOpponent(StrategyBackup, PlaceStrategy):
     """
     Place the worker on a field that is as far away from the 
     other player’s worker(s) using the notion of geometric 
     distance on a Cartesian plane.
     """
+
+
+    def strategize(self, board, workers, rules):
+        workers_on_board = list(map(lambda w: w.id, board.workers))
+        opponent_workers = list(filter(lambda w: not w in workers, workers_on_board))
+        opponent_worker_positions = list(map(lambda w: board.get_worker_positon(w), opponent_workers))
+
+        width, height = board.dimensions
+
+        farthest_point = None
+        for x in range(width):
+            for y in range(height):
+                if rules.check_place(x, y) and self._total_distance((x, y), opponent_workers) >= self._total_distance(farthest_point, opponent_workers):
+                    farthest_point = (x , y)
+
+        if farthest_point is None:
+            raise ValueError("Strategy not applicable")
     
-    workers_on_board = list(map(lambda w: w.id, board.workers))
-    opponent_workers = list(filter(lambda w: not w in workers, workers_on_board))
-    opponent_worker_positions = list(map(lambda w: board.get_worker_positon(w), opponent_workers))
+        return farthest_point
+        
+    def _total_distance(from_cell, to_cells):
+        if from_cell is None: return 0
+        return reduce(lambda x, y: x + self._cartesian_distance(from_cell, y), to_cells, 0)
 
-    width, height = board.dimensions
+    def _cartesian_distance(from_cell, to_cell):
+        return hypot(to_cell[0] - from_cell[0], to_cell[1] - from_cell[1])
 
-    farthest_point = None
-    for x in range(width):
-        for y in range(height):
-            if rules.check_place(x, y) and total_distance((x, y), opponent_workers) >= total_distance(farthest_point, opponent_workers):
-                farthest_point = (x , y)
-
-    if farthest_point is None:
-        raise ValueError("Strategy not applicable")
-    
-    return farthest_point
-
-def total_distance(from_cell, to_cells):
-    if from_cell is None: return 0
-    return reduce(lambda x, y: x + cartesian_distance(from_cell, y), to_cells, 0)
-
-def cartesian_distance(from_cell, to_cell):
-    return hypot(to_cell[0] - from_cell[0], to_cell[1] - from_cell[1])
--- Santorini/Design/strategy.py
+++ Santorini/Design/strategy.py
@@ -43,55 +43,81 @@ class IStrategy(ABC):
         pass
 
 
-class PlaceStrategy(ABC):
+
+class TurnStrategy(ABC):
+    """
+    Santorini strategy to plan for next turn.
+
+    TurnSpecification is one of:
+    - (N, N)
+    - (N, Direction)
+    - Direction
+
+    """
+
+    @abstractmethod
+    def strategize(self, board, workers, rules):
+        """
+        Apply the strategy to produce specifications for the next turn
+
+        :param board: IQueryBoard, the current board of the game
+        :param workers: [N, ...], list of worker ids
+        :param rules: IRules, the rule checker with game's rules
+        :return: TurnSpecification, specification for the turn
+        """
+        pass 
+
+
+
+class PlaceStrategy(TurnStrategy):
     """
     Santorini strategy to plan for next worker place action for a player.
     """
 
     @abstractmethod
-    def apply(self, board, workers, rules):
-        """
-        Apply the strategy to produce specifications for the next move. 
-
-        :param board: IQueryBoard, the current board of the game
-        :param workers: [N, ...], list of worker ids of player to be placed
-        :param rules: IRules, the rule checker with standard Santorini rules
-        :return: (N, N), the x and y coordinates to place a new worker
-        """
-        pass 
-          
-
-class MoveStrategy(ABC):
-    """
-    Santorini strategy to plan for next action for a player.
-    """
-
-    @abstractmethod
-    def apply(self, board, workers, rules): 
+    def strategize(self, board, workers, rules):
         """
         Apply the strategy to produce specifications for the next move. 
 
         :param board: IQueryBoard, the current board of the game
         :param workers: [N, ...], list of worker ids of the player 
-        :param rules: Rules, the rule checker with standard Santorini rules
+        :param rules: IRules, the rule checker with game's rules
+        :return: (N, N), the x and y coordinates to place a new worker
+        """
+        pass 
+          
+
+class MoveStrategy(TurnStrategy):
+    """
+    Santorini strategy to plan for next action for a player.
+    """
+
+    @abstractmethod
+    def strategize(self, board, workers, rules): 
+        """
+        Apply the strategy to produce specifications for the next move. 
+
+        :param board: IQueryBoard, the current board of the game
+        :param workers: [N, ...], list of worker ids of the player 
+        :param rules: Rules, the rule checker with game's rules
         :return: (N, Direction), the worker id, move direction
         """
         pass
 
 
-class BuildStrategy(ABC):
+class BuildStrategy(TurnStrategy):
     """
     Build strategy in a Santorini game.
     """
 
     @abstractmethod
-    def apply(self, board, worker, rules): 
+    def strategize(self, board, workers, rules): 
         """
         Apply the strategy to produce specifications for next build.
 
         :param board: IQueryBoard, the current board of the game
-        :param worker: N, id of worker to build with 
-        :param rules: Rules, the rule checker with standard Santorini rules
+        :param worker: [N], id of worker to build with in a list
+        :param rules: Rules, the rule checker with game's rules
         :return: Direction, build direction
         """
         pass
