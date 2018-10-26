f3b857fb4b7735ca7f5cf9ad480c0d22876a158b
gvjacob <gvjacob@outlook.com>
2018-10-26 00:04:08 -0400

    iterator and referee test

---
+++ Santorini/Admin/tests/test_continuous_iterator.py
@@ -0,0 +1,34 @@
+import pytest
+
+from Santorini.Admin.referee import ContinuousIterator
+
+@pytest.fixture
+def items():
+    return [1, 2, 3]
+
+
+@pytest.fixture
+def iterator(items):
+    return iter(ContinuousIterator(items))
+
+
+@pytest.fixture
+def empty_iterator():
+    return iter(ContinuousIterator([]))
+
+
+def test_empty_list(empty_iterator):
+    for i in empty_iterator:
+        assert i is not None
+
+
+def test_iterate_at_least_once(items, iterator):
+    for i, j in zip(items, iterator):
+        assert i is j
+
+
+def test_iterator_loop_around(items, iterator):
+    items = items * 10
+    for i, j in zip(items, iterator):
+        assert i is j
+
---
+++ Santorini/Admin/tests/test_referee.py
@@ -0,0 +1,35 @@
+import pytest
+
+from Santorini.Admin.referee import SantoriniReferee
+from Santorini.Player.player import Player
+from Santorini.Common.rule_checker import RuleChecker
+
+@pytest.fixture
+def player_one():
+    return Player(1)
+
+@pytest.fixture
+def player_two():
+    return Player(2)
+
+@pytest.fixture
+def referee(player_one, player_two):
+    return SantoriniReferee(player_one, player_two, RuleChecker)
+
+
+class TestInit:
+    def test_players(self, player_one, player_two, referee):
+        assert referee.players == [player_one, player_two]
+
+    def test_only_two_players(self, referee):
+        assert len(referee.players) is 2
+
+
+class TestRunGames:
+    def test_run_single(self, referee):
+        print(referee.run_games())
+
+
+
+
+
